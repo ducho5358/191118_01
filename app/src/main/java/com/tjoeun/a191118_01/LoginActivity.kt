4d7d4856e7197ac19b051561ef51f0664bbc79d3
ducho <ducho5358@gmail.com>
2019-11-18 20:14:42 +0900

    로그인 화면

--- app/src/main/java/com/tjoeun/a191118_01/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/a191118_01/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.tjoeun.a191118_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity : BaseActivity() {
 
@@ -9,7 +11,7 @@ class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_Login)
         setupEvents()
         setValues()
 
@@ -18,6 +20,14 @@ class LoginActivity : BaseActivity() {
 
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+            if(rememberIdCheckBox.isChecked) {
+                Toast.makeText(mContext, "아이디를 저장해야 합니다.", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(mContext, "아이디 저장 X", Toast.LENGTH_SHORT).show()
+            }
 
     }
 
